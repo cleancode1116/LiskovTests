cac57bdfa63da7719c56e111cf1f9c145daddab1
Administrator <Administrator@raum02pc01>
2016-11-16 11:14:32 +0100

    More tests

--- test/de/integrata/oop/liskov/IdentityTest.java
+++ test/de/integrata/oop/liskov/IdentityTest.java
@@ -1,5 +1,11 @@
 package de.integrata.oop.liskov;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class IdentityTest extends CrypterTest<IdentityCrypter> {
 	
 	@Override
@@ -7,5 +13,13 @@ public class IdentityTest extends CrypterTest<IdentityCrypter> {
 		return new IdentityCrypter();
 	}
 	
-
+	@Test
+	public void cryptReturnsSameValue() {
+		assertThat(crypter.crypt("Hallo"), is(equalTo("Hallo")));
+	}
+	
+	@Test
+	public void decryptReturnsSameValue() {
+		assertThat(crypter.decrypt("Hallo"), is(equalTo("Hallo")));
+	}
 }
